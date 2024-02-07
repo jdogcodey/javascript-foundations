93585280c9fb247141cb3c86fc07fdfabb1caf03
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 23:07:55 +0000

    Completed DOM manipulation exercise 1
    Found this one not too difficult. Not sure why the black border doesn't
    show up but I think it is there.

---
+++ foundations-dom-manipulation-and-events/script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+container.appendChild(content);
+
+
+const para = document.createElement('p');
+para.textContent = "Hey I'm red!";
+para.style.color = 'red';
+container.appendChild(para);
+
+const title = document.createElement('h3');
+title.textContent = "I'm a blue h3!";
+title.style.color = 'blue';
+container.appendChild(title);
+
+const blackPink = document.createElement('div');
+blackPink.style.borderColor = 'black';
+blackPink.style.backgroundColor = 'pink';
+
+const para2 = document.createElement('h1');
+para2.textContent = "I'm in a div!";
+blackPink.appendChild(para2);
+
+const para3 = document.createElement('p');
+para3.textContent = "ME TOO!";
+blackPink.appendChild(para3);
+
+container.appendChild(blackPink);
+
+
