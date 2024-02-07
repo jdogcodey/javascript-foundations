f1336ac9b33a1b50778d7a9cc03673103acf7f2c
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-07 13:45:10 +0000

    Finished the MDN Countdown
    Took a few goes to get this right - made a few infinite loops but
    understand the reasoning for this now.
    
    Interesting to note that I did this using a for loop while MDN did this
    using a while loop. Don't think it makes a difference to the outcome
    at all but good to know that there are different ways to do things.

---
+++ mdn-looping-code/script.js
@@ -0,0 +1,16 @@
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+for (let i = 10; i >= 0; i --) {
+    const para = document.createElement('p');
+    if (i === 10)  {
+    para.textContent = 'Countdown 10';
+    }
+    else if (i === 0) {
+    para.textContent = 'Blastoff!';
+    }
+    else { 
+    para.textContent = i;
+    }
+    output.appendChild(para);
+    } 
